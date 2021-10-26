f3afaf700f6a3f8bfa27722d3793d0493830256d
Selyanyn <56536978+Selyanyn@users.noreply.github.com>
2021-10-26 19:19:41 +0500

    SEL 26.10: Добавил метод для чтения профиля

--- backend/api/profile.py
+++ backend/api/profile.py
@@ -1,5 +1,6 @@
+import base64
 from rest_framework.decorators import api_view
-from .models import User
+from .models import User, Profile
 from .crypto import gen_token
 from rest_framework.response import Response
 
@@ -14,7 +15,8 @@ def get_current_user(request):
         return Response(data={'message': 'Вы не авторизовались'}, status=400)
     token = request.session['token']
     user = User.objects.filter(token=token)[0]
-    return Response(data={'username': user.username,
+    return Response(data={'id': user.id,
+                          'username': user.username,
                           'phone': user.phone,
                           'sbis': user.sbis}, status=200)
 
@@ -26,6 +28,29 @@ def get_user_info(request):
         Чтобы получить данные авторизованного пользователя, используйте get_current_user
     """
     pass
+
+
+@api_view(['GET'])
+def get_profile_info(request):
+    """
+        Селянин 26.10:
+        Метод для чтения профиля по id. Используется для получения данных для личной
+        странички пользователя.
+    """
+    if 'token' not in request.session.keys():
+        return Response(data={'message': 'Вы не авторизовались'}, status=400)
+    token = request.session['token']
+    user = User.objects.filter(token=token)[0]
+    profile = user.profile
+    with open(f'../../files/{profile.photo}.jpg', "rb") as image_file:
+        photo_data = base64.b64encode(image_file.read()).decode('utf-8')
+    return Response(data={'id': user.id,
+                          'username': user.username,
+                          'phone': user.phone,
+                          'sbis': user.sbis,
+                          'photo': photo_data
+                          }, status=200)
+
 
 
 @api_view(['GET'])
