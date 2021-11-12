255ee00a4e688e8095eb363699e586f11227bf1f
snooperv <snooperv@gmail.com>
2021-11-12 17:34:20 +0500

    Init branch

--- api/api/settings.py
+++ api/api/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'perforetor/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'perforator/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
