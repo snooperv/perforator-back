8b8dc4cf26a1c4b89a9c525e054825ffc33a2f47
Selyanyn <56536978+Selyanyn@users.noreply.github.com>
2021-11-13 15:49:38 +0500

    SEL 13.11: исправил методы, пофиксил баги, добавил комментариев

--- api/perforator/reviews.py
+++ api/perforator/reviews.py
@@ -10,8 +10,6 @@ from django.conf import settings
 def __format_review_data(review):
     result = {
         'id': review.id,
-        'appraising_person': review.appraising_person_id,
-        'evaluated_person': review.evaluated_person,
         'is_draft': review.is_draft,
         'grades': []
     }
@@ -22,7 +20,6 @@ def __format_review_data(review):
             'grade_category_id': grade.grade_category.id,
             'grade_category_name': grade.grade_category.name,
             'grade_category_description': grade.grade_category.description,
-            'grade': grade.grade,
             'comment': grade.comment,
         })
     return result
@@ -31,7 +28,16 @@ def __format_review_data(review):
 def get_self_review(request):
     """
         Возвращает селф-ревью
-        Аналогичен возвращению обыкновенного ревью, вынесем функционал в другое место
+        request.GET: параметры не нужны
+        :return: Один из следующих словраей:
+        { 'id': review.id, 'is_draft': review.is_draft,
+        'grades':   [
+            { 'id': grade.id, 'grade_category_id': grade.grade_category.id,
+            'grade_category_name': grade.grade_category.name, 'grade_category_description': grade.grade_category.description,
+            'comment': grade.comment (только коммент. У сэлф-ревью оценка всегда NULL),
+            } {...}, {...}
+        ] },
+        При ошибке: {'error': True, 'message': 'Профиль с таким id не найден'}
     """
     if not request.user.is_authenticated:
         return {'error': True, 'message': 'Вы не авторизовались'}
@@ -59,6 +65,17 @@ def edit_self_review(request):
     """
         Изменяет селф-ревью
         Аналогичен изменению обыкновенного ревью, вынесем функционал в другое место
+        request.data: (Заполненное селф-ревью)
+        { 'id': review.id, 'is_draft': review.is_draft,
+        'grades':   [
+            {
+            'grade_category_id': grade.grade_category.id,
+            'comment': grade.comment (только коммент. У сэлф-ревью оценка всегда NULL),
+            }, {...}, {...}
+        ] }
+        :return: Один из следующих словраей:
+        { message: "ОК" },
+        При ошибке: {'error': True, 'message': 'Профиль с таким id не найден'}
     """
     if not request.user.is_authenticated:
         return {'error': True, 'message': 'Вы не авторизовались'}
@@ -69,7 +86,7 @@ def edit_self_review(request):
     for grade in request.data['grades']:
         review.objects \
             .filter(grade__grade_category__id=grade['grade_category_id']) \
-            .update(grade=grade['grade'], comment=grade['comment'])
+            .update(grade=None, comment=grade['comment'])
     if not request.data['is_draft']:
         review.is_draft = False
     review.save()
@@ -79,21 +96,48 @@ def edit_self_review(request):
 def get_empty_review_form(request):
     """
         Возвращает запрошенный набор тем для построения формы ревью
-        Требуется роль оцениваемых (менеджер \ пиры \ команда)
         Так как у обыкновенных ревью черновиков нет, возвращать готовые ревью не придётся
         Список не оценённых менеджеров \ пиров \ подчинённых запрашивается отедльно
+        request.GET: параметры не нужны
+        :return: Один из следующих словраей:
+        { categories:
+            manager_review_categories: [
+                { id, name, description },
+                {...}, {...}
+            ],
+            peers_review_categories: [ {...}, {...}],
+            team_review_categories: [ {...}, {...}],
+         },
+        При ошибке: {'error': True, 'message': 'Профиль с таким id не найден'}
     """
-    categories = []
-    if request.GET.get('evaluated_person') in ['manager', 'peers', 'team']:
-        categories = getattr(PerformanceReview.objects.get(id=settings.PERFORMANCE_REVIEW_ID),
-                             request.GET.get('evaluated_person') + '_review_categories')
-    return categories.values('id', 'name', 'description')
+    performance_review = PerformanceReview.objects.get(id=settings.PERFORMANCE_REVIEW_ID)
+    categories = {}
+    for role in ['manager', 'peers', 'team']:
+        categories_name = role + '_review_categories'
+        categories[categories_name] = getattr(performance_review, categories_name).values('id', 'name', 'description')
+    return categories
 
 
 def save_review(request):
     """
         Сохраняет ревью на выбранного пользователя
         Сохранять можно только в чистовик, то есть возвращать его больше не придётся
+        request.data: (Заполненное ревью. Аналогично селф-ревью, но:
+        все оценки имеют числовой эквивалент;
+        is_draft автоматически ставится false;
+        есть возможность поставить not_enough_data (если пользователь не может оценить человека))
+        { 'id': review.id,
+        'evaluated_person': review.evaluated_person_id,
+        'is_not_enough_data': boolean
+        'grades':   [
+            { 'id': grade.id, 'grade_category_id': grade.grade_category.id,
+            'grade': grade.grade,
+            'comment': grade.comment,
+            }, {...}, {...}
+        ] }
+        :return: Один из следующих словраей:
+        { message: "ОК" },
+        При ошибке: {'error': True, 'message': 'Профиль с таким id не найден'}
     """
     if not request.user.is_authenticated:
         return {'error': True, 'message': 'Вы не авторизовались'}
@@ -102,7 +146,7 @@ def save_review(request):
                     evaluated_person_id=int(request.data['evaluated_person_id']),
                     performance_review_id=settings.PERFORMANCE_REVIEW_ID,
                     is_draft=False)
-    if not request.data['not_enough_data']:
+    if not request.data['is_not_enough_data']:
         for grade in request.data['grades']:
             Grade.objects.create(review=review,
                                  grade_category_id=grade['grade_category_id'],
