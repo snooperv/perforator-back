111f8ea81690bd626804542ebce067e0041156cc
SeriousDim <dlykov2001@yandex.ru>
2021-12-08 23:43:42 +0500

    ratings.py done

--- api/perforator/ratings.py
+++ api/perforator/ratings.py
@@ -15,19 +15,26 @@ def create_review_form(request):
     return RateForm()
 
 
-def get_user_review(request, profile):
+def get_review_from_current_user(request, profile_rated):
+    if request.user.is_authenticated:
+        cur_profile = Profile.objects.filter(user=request.user)[0]
+        return get_review_to_profile(request, cur_profile, profile_rated)
+    else:
+        return {"error": 'Вы не авторизованы'}
+
+
+def get_review_to_profile(request, profile_from, profile_rated):
     """
         Возвращает запись в таблицы PeerReviews -
-        ревью залогиненного пользовтаеля по отн-ю к
-        пользователю profile
-        profile - запись из модели Profile
+        ревью от пользователя profile_from по отн-ю к
+        пользователю profile_rated
+        profile_from, profile_rated - запись из модели Profile
         :return: словарь с данными (ключи называются также,
         как и в моделе PeerReviews) или ошибкой
     """
     if request.user.is_authenticated:
-        cur_profile = Profile.objects.filter(user=request.user)[0]
-        review = PeerReviews.objects.filter(rated_person=profile) \
-            .filter(peer_id=cur_profile).first()
+        review = PeerReviews.objects.filter(rated_person=profile_rated) \
+            .filter(peer_id=profile_from).first()
         answer = {}
         if review is not None:
             answer = dict(review)
@@ -39,16 +46,21 @@ def get_user_review(request, profile):
         return {"error": 'Вы не авторизованы'}
 
 
-def gen_user_review_form(request, profile):
+def generate_review_form_from_current_user(request, profile_rated):
+    if request.user.is_authenticated:
+        cur_profile = Profile.objects.filter(user=request.user)[0]
+        return generate_review_form(request, cur_profile, profile_rated)
+    else:
+        return {"error": 'Вы не авторизованы'}
+
+
+def generate_review_form(request, profile_from, profile_rated):
     """
-        Возврашает RateForm, созданную залогинненым пользователем
-        по отношению к пользователю profile
-        profile - запись из модели Profile
         :return: форму из таблицы PeerReviews, новая пустая форма или
         словарь с ошибкой
     """
     if request.user.is_authenticated:
-        review = get_user_review(request, profile)
+        review = get_review_to_profile(request, profile_from, profile_rated)
 
         if review['created'] == False:
             return create_review_form(request)
@@ -60,30 +72,55 @@ def gen_user_review_form(request, profile):
         return {"error": 'Вы не авторизованы'}
 
 
-def save_user_review_form(request, profile, form):
+def save_review_form_from_current_user(request, profile_rated, form):
+    if request.user.is_authenticated:
+        cur_profile = Profile.objects.filter(user=request.user)[0]
+        return save_review_form(request, cur_profile, profile_rated, form)
+    else:
+        return {"error": 'Вы не авторизованы'}
+
+
+def save_review_form(request, profile_from, profile_rated, form):
     """
-        Сохранить форму form, созданную залогиненным пользователем
-        по отношению к пользовтаелю profile в таблицу PeerReviews
-        profile - запись из модели Profile
-        form - RateForm
         :return: словарь с ответом или ошибкой
     """
     if request.user.is_authenticated:
         if form.is_valid():
-            cur_profile = Profile.objects.filter(user=request.user)[0]
+            review, created = PeerReviews.objects \
+                .update_or_create(peer_id=profile_from,
+                                  rated_person=profile_rated,
+                                  defaults=form.cleaned_data)
+            review.save()
+            return {'message': 'OK'}
+        else:
+            return {'error': 'Невалидная форма', 'form': form}
+    else:
+        return {"error": 'Вы не авторизованы'}
 
-            new_review = PeerReviews.objects.create(peer_id=1,
-                                                    rated_person=1,
-                                                    **form.cleaned_data)
+
+def generate_matched_profiles_and_forms_from_current_user(request):
+    if request.user.is_authenticated:
+        cur_profile = Profile.objects.filter(user=request.user)[0]
+        return generate_matched_profiles_and_forms(request, cur_profile)
+    else:
+        return {"error": 'Вы не авторизованы'}
 
 
-def gen_matched_users_and_forms(request):
+def generate_matched_profiles_and_forms(request, profile_from):
     """
-        Создать или взять из таблицы PeerReviews форму RateForm для всех
-        пользователей, у которых залогиненный пользователь является пиром
         :return: словарь след. вида:
         { profile_object: rate_form, ... }
         или словарь с ошибкой
     """
-    pass
+    if request.user.is_authenticated:
+        reviews = PeerReviews.objects.filter(peer_id=profile_from)
+        if (len(reviews) == 0):
+            return {}
 
+        answer = {}
+        for r in reviews:
+            form = generate_review_form(request, profile_from, r.rated_person)
+            answer[r.rated_person] = form
+        return answer
+    else:
+        return {"error": 'Вы не авторизованы'}
