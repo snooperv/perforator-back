2130163f27091501c4f9a1feb537e9dfd88f9c74
Nikita <kitrs2001@gmail.com>
2021-12-09 13:57:10 +0500

    пофиксил баг кнопки "я менеджер"

--- api/perforator/static/js/disable_button.js
+++ api/perforator/static/js/disable_button.js
@@ -1,18 +1,13 @@
 
 window.onload = function () {
-    get_team()
+    fetch(window.location.origin + "/perforator/team")
         .then(response => response.json())
         .then(json => {
             let link = document.getElementById('i_manager_button')
             if (!json.length >= 1){
-                link.href = ''
-                link.classList.add("imanager_disable")
+                link.style.display = "none";
             }
             console.log(json.length)
             console.log(link.href)
         })
 };
-
-function get_team(){
-    return fetch(window.location.origin + "/perforator/team");
-}
