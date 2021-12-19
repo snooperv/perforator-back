1622cc4713d67797e6b6f4141f6e362a11fb0a95
Nikita <kitrs2001@gmail.com>
2021-12-19 17:12:03 +0500

    добавил статистику команды на страницу я менеджер

--- api/perforator/static/js/i_rate_perforatoriki.js
+++ api/perforator/static/js/i_rate_perforatoriki.js
@@ -5,36 +5,27 @@ let rad4 = document.formRate[0].rates_practices;
 let rad5 = document.formRate[0].rates_experience;
 let rad6 = document.formRate[0].rates_adaptation;
 let prev = null;
-let rates = ['Значительно ниже моих ожиданий', 'Немного ниже моих ожиданий',
-    'Немного выше моих ожиданий', 'Значительно выше моих ожиданий'];
-
-for (let i = 0; i < rad1.length; i++) {
-    rad1[i].value = rates[rad1.length - i - 1];
-    rad2[i].value = rates[rad1.length - i - 1];
-    rad3[i].value = rates[rad1.length - i - 1];
-    rad4[i].value = rates[rad1.length - i - 1];
-    rad5[i].value = rates[rad1.length - i - 1];
-    rad6[i].value = rates[rad1.length - i - 1];
-}
+let rates = ['Значительно выше моих ожиданий', 'Немного выше моих ожиданий',
+    'Немного ниже моих ожиданий', 'Значительно ниже моих ожиданий'];
 
 for (let i = 0; i < rad1.length; i++) {
     rad1[i].onclick = function () {
-        document.getElementById('text_rate1').innerHTML = this.value;
+        document.getElementById('text_rate1').innerHTML = rates[rad1[i].value - 1];
     };
     rad2[i].onclick = function () {
-        document.getElementById('text_rate2').innerHTML = this.value;
+        document.getElementById('text_rate2').innerHTML = rates[rad1[i].value - 1];
     };
     rad3[i].onclick = function () {
-        document.getElementById('text_rate3').innerHTML = this.value;
+        document.getElementById('text_rate3').innerHTML = rates[rad1[i].value - 1];
     };
     rad4[i].onclick = function () {
-        document.getElementById('text_rate4').innerHTML = this.value;
+        document.getElementById('text_rate4').innerHTML = rates[rad1[i].value - 1];
     };
     rad5[i].onclick = function () {
-        document.getElementById('text_rate5').innerHTML = this.value;
+        document.getElementById('text_rate5').innerHTML = rates[rad1[i].value - 1];
     };
     rad6[i].onclick = function () {
-        document.getElementById('text_rate6').innerHTML = this.value;
+        document.getElementById('text_rate6').innerHTML = rates[rad1[i].value - 1];
     };
 }
 
