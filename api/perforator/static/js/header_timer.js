3c8cdeaeac308fa3eecb4d14d61ad440bf10dfb8
Vadim <snooperv@gmail.com>
2022-01-17 14:34:20 +0500

    Change timer in header

--- api/perforator/static/js/header_timer.js
+++ api/perforator/static/js/header_timer.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Jan 10, 2022 23:00:00").getTime();
+var countDownDate = new Date("Jan 31, 2022 23:00:00").getTime();
 var x = setInterval(function () {
     var now = new Date().getTime();
     var distance = countDownDate - now;
