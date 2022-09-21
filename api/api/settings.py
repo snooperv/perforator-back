ecc4773acf0ae68a87d50bfaeb0ed8cbd34dceb4
snooperv <snooperv@gmail.com>
2022-09-21 14:43:12 +0500

    Добавление сваггера

--- api/api/settings.py
+++ api/api/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'widget_tweaks',
     'rest_framework',
     'perforator',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- api/api/urls.py
+++ api/api/urls.py
@@ -1,15 +1,39 @@
 from django.views.generic import RedirectView
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
+
     path('admin/', admin.site.urls),
     path('perforator/', include('perforator.urls')),
     path('', RedirectView.as_view(url='/perforator/', permanent=True)),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/login/registration', RedirectView.as_view(url='/perforator/registration/'))
+    path('accounts/login/registration',
+         RedirectView.as_view(url='/perforator/registration/'))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
