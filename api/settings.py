776b64893c0d83895be5a1fefb0db4ccf314e8bb
snooperv <snooperv@gmail.com>
2022-09-21 20:09:21 +0500

    Исправление подключения JWT токена

--- api/settings.py
+++ api/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'api.urls'
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
 }
 
 TEMPLATES = [
